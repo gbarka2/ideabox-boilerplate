26cdf1cbe19cc2e0f81db5d0e441606e42ad02bd
Gary Barkauski <gbarka2@gmail.com>
2020-10-30 10:58:30 -0600

    Add clearEntries function to clear the title/body

--- src/main.js
+++ src/main.js
@@ -1,4 +1,15 @@
-var titleAndBody = document.querySelector('.form-entries');
+var title = document.querySelector('#title');
+var body = document.querySelector('#body');
+
+var titleAndBody = document.querySelectorAll('.form-entries');
 var saveButton = document.querySelector('.save-button');
 var ideaBox = document.querySelector('.idea-box');
 var ideaList = [];
+
+saveButton.addEventListener('click', clearEntries)
+
+function clearEntries() {
+  if (titleAndBody.value) {
+    titleAndBody.value = null;
+  }
+}
