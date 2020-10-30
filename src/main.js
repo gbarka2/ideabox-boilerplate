50f62a770f9ed0ed4958c460bce7fec95a6d05b4
Gary Barkauski <gbarka2@gmail.com>
2020-10-30 11:13:34 -0600

    Attempt to refactor to one variable in progress

--- src/main.js
+++ src/main.js
@@ -1,3 +1,6 @@
+var title = document.querySelector('#title');
+var body = document.querySelector('#body');
+
 var titleAndBody = document.querySelectorAll('.form-entries');
 var saveButton = document.querySelector('.save-button');
 var ideaBox = document.querySelector('.idea-box');
@@ -5,8 +8,21 @@ var ideaList = [];
 
 saveButton.addEventListener('click', clearEntries)
 
+///WORKING FUNCTION///
 function clearEntries() {
-  if (titleAndBody.value) {
-    titleAndBody.value = null;
+  debugger
+  if (title.value && body.value) {
+    title.value = null;
+    body.value = null;
   }
 }
+
+// 
+// function clearEntries() {
+//   debugger
+//   if (title.value && body.value) {
+//     titleAndBody.value = null;
+//     //     title.value = null;
+//     //     body.value = null;
+//   }
+// }
