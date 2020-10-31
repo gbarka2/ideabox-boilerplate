7768fe676862bc13f0349f63199024e052767048
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-10-31 11:39:45 -0400

    Declare favoriteIdea function

--- src/main.js
+++ src/main.js
@@ -5,23 +5,14 @@ var titleAndBody = document.querySelectorAll('.form-entries');
 var saveButton = document.querySelector('.save-button');
 var ideaSection = document.querySelector('.grid-item-3');
 var disableHover = document.querySelector('.disable-hover');
+var inactiveStar = document.querySelector('#favorite-star');
+var activeStar = document.querySelector('#active-star');
+
 var ideaList = [];
 var currentIdea;
 
 saveButton.addEventListener('click', newIdeaCard);
 disableHover.addEventListener('mouseenter', enableButton);
-
-///WORKING FUNCTION///
-// LOOKING TO REFACTOR USING titleAndBody ONLY
-function clearEntries() {
-  if (title.value && body.value) {
-    title.value = null;
-    body.value = null;
-    saveButton.disabled = true;
-    saveButton.classList.add('disable-button');
-    // possible add of "add('disable-button') function"
-  }
-};
 
 function enableButton() {
   if (title.value && body.value) {
@@ -37,6 +28,7 @@ function updateIdeaList() {
     ideaList.push(currentIdea);
   }
 };
+//is there a way to manipulate below without having all the html? innerText of the h3 and p?
 
 function newIdeaCard() {
   updateIdeaList();
@@ -46,7 +38,7 @@ function newIdeaCard() {
     <article class="idea-box">
       <div class="icon-bar">
         <img src="assets/star.svg" class="idea-images" id="favorite-star" alt="favorite star">
-        <img src="assets/star-active.svg" class="idea-images" id="active-star" alt="favorited star" hidden>
+        <img src="assets/star-active.svg" class="idea-images hidden" id="active-star" alt="favorited star">
         <img src="assets/delete.svg" class="idea-images" id="delete-idea" alt="delete idea">
         <img src="assets/delete-active.svg" class="idea-images hidden" id="active-delete" alt="please delete idea">
       </div>
@@ -60,4 +52,33 @@ function newIdeaCard() {
       </div>
     </article>`
   }
+
+  ///WORKING FUNCTION///
+  // LOOKING TO REFACTOR USING titleAndBody ONLY
+  function clearEntries() {
+    if (title.value && body.value) {
+      title.value = null;
+      body.value = null;
+      saveButton.disabled = true;
+      saveButton.classList.add('disable-button');
+      // possible add of "add('disable-button') function"?
+    }
+  };
 };
+
+
+inactiveStar.addEventListener('click', favoriteIdea);
+console.log('favorite star', inactiveStar);
+//ok
+console.log('active favorite star', activeStar);
+//error message of "cannot read property of null" line16
+function favoriteIdea() {
+  alert('it fired!');
+  // inactiveStar.classList.add('hidden');
+}
+
+//I tried using .setAttribute/removeAttribute
+//I tried setting activeStar.hidden = false
+//I tried using hidden as an attribute and then hidden as a class value and applying the CSS property of display:none;
+//The error of the event listener being null seems to be the biggest road block.
+//some comments on stackoverflow indicate it has to do with the DOM not being loaded before it fires? 
