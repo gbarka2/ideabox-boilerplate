7ae21e0b3b59c2df87216750ff38ebb21786decc
Gabrielle Joyce <gabriellejoyce1930@gmail.com>
2020-11-01 17:02:35 -0500

    Create manageIdeaBox function

--- src/main.js
+++ src/main.js
@@ -5,15 +5,14 @@ var titleAndBody = document.querySelectorAll('.form-entries');
 var saveButton = document.querySelector('.save-button');
 var ideaSection = document.querySelector('.grid-item-3');
 var disableHover = document.querySelector('.disable-hover');
-var inactiveStar = document.querySelector('#favorite-star');
-var activeStar = document.querySelector('#active-star');
 
 var ideaList = [];
 var currentIdea;
 
 saveButton.addEventListener('click', newIdeaCard);
 disableHover.addEventListener('mouseenter', enableButton);
-ideaSection.addEventListener('click', deleteIdeaBox);
+// ideaSection.addEventListener('click', deleteIdeaBox);
+ideaSection.addEventListener('click', manageIdeaBox);
 
 function enableButton() {
   if (title.value && body.value) {
@@ -41,7 +40,9 @@ function updateIdeaList() {
     ideaList.push(currentIdea);
   }
 };
+
 //is there a way to manipulate below without having all the html? innerText of the h3 and p?
+//see slack for questions on active-delete & hover
 
 function newIdeaCard() {
   updateIdeaList();
@@ -50,8 +51,8 @@ function newIdeaCard() {
     ideaSection.innerHTML += `
     <article class="idea-box" id="${ideaList[i].id}">
       <div class="icon-bar">
-        <img src="assets/star.svg" class="idea-images" id="favorite-star" alt="favorite star">
-        <img src="assets/star-active.svg" class="idea-images hidden" id="active-star" alt="favorited star">
+        <img src="assets/star.svg" class="idea-images white-star" id="${ideaList[i].id}" alt="favorite star">
+        <img src="assets/star-active.svg" class="idea-images red-star hidden" id="${ideaList[i].id}" alt="favorited star">
         <img src="assets/delete.svg" class="idea-images delete-idea" id="${ideaList[i].id}" alt="delete idea">
         <img src="assets/delete-active.svg" class="idea-images hidden" id="active-delete" alt="please delete idea">
       </div>
@@ -67,9 +68,20 @@ function newIdeaCard() {
   }
 };
 
+function manageIdeaBox(event) {
+  debugger;
+  if (event.target.className === 'delete-idea') {
+    deleteIdeaBox();
+  } else if (event.target.className === 'white-star' || event.target.className === 'red-star') {
+    favoriteIdeaBox();
+  } else {
+    alert('WRONG');
+  }
+}
+
 function deleteIdeaBox(event) {
   var ideaID = Number(event.target.id);
-  for(var i = 0; i < ideaList.length; i++) {
+  for (var i = 0; i < ideaList.length; i++) {
     var article = document.getElementById(ideaID);
     if (ideaID === ideaList[i].id) {
       ideaList.splice(i, 1);
@@ -78,18 +90,34 @@ function deleteIdeaBox(event) {
   }
 };
 
-// inactiveStar.addEventListener('click', favoriteIdea);
-// console.log('favorite star', inactiveStar);
-// //ok
-// console.log('active favorite star', activeStar);
-// //error message of "cannot read property of null" line16
-// function favoriteIdea() {
-//   alert('it fired!');
-//   // inactiveStar.classList.add('hidden');
-// }
+function favoriteIdeaBox(event) {
+  var ideaID = Number(event.target.id);
+  var whiteStar = document.querySelector('.white-star');
+  var redStar = document.querySelector('.red-star');
+  for (var i = 0; i < ideaList.length; i++) {
+    // var article = document.getElementById(ideaID);
+    // need for later?
+    if (ideaID === ideaList[i].id) {
+      whiteStar.classList.toggle('hidden');
+      redStar.classList.toggle('hidden');
+      // ideaList[i].star = true;
+      //how do we get it to unfavor then?
+    }
+  }
+}
 
-//I tried using .setAttribute/removeAttribute
-//I tried setting activeStar.hidden = false
-//I tried using hidden as an attribute and then hidden as a class value and applying the CSS property of display:none;
-//The error of the event listener being null seems to be the biggest road block.
-//some comments on stackoverflow indicate it has to do with the DOM not being loaded before it fires?
+// function toggleStar(star){
+//   if()
+//   whiteStar.classList.toggle('hidden');
+//   redStar.classList.toggle('hidden');
+// }
+//how are we accessing the DOM? --> via the parent box of ideaSection
+
+//if/else statement to assess if user wants to favorite or to delete? That way not crossing too many paths with ideaSection.
+
+
+// function favoriteIdeaBox() {
+//create a getElementById for favorite-star and active-star
+//if the event.target.id is the same as the ideaList[i].id
+//classList.add/remove hidden
+// }
