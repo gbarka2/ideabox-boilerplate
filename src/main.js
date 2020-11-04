4db92d257ab0df435d672c04a7fdb3b3ef89488d
Cameron Aragon <caragon4695@gmail.com>
2020-11-04 14:52:45 -0700

    Refactor innerHTML to one function

--- src/main.js
+++ src/main.js
@@ -41,24 +41,7 @@ function updateIdeaList() {
 
 function newIdeaCard() {
   updateIdeaList();
-  ideaSection.innerHTML = '';
-  for (var i = 0; i < ideaList.length; i++) {
-    ideaSection.innerHTML += `
-    <article class="idea-box" id="${ideaList[i].id}">
-      <div class="icon-bar">
-        <img src="assets/star.svg" class="white-star" id="${ideaList[i].id}" alt="favorite star">
-        <img src="assets/delete.svg" class="delete-idea" id="${ideaList[i].id}" alt="delete idea">
-      </div>
-      <div class="title-body">
-        <h3>${ideaList[i].title}</h3>
-        <p>${ideaList[i].body}</p>
-      </div>
-      <div class="comment-bar">
-        <img src="assets/comment.svg" class="comment" id="add-comment" alt="add comment">
-        <h4>Comment</h4>
-      </div>
-    </article>`
-  }
+  reassignHTML();
 };
 
 function manageIdeaBox(event) {
@@ -100,24 +83,7 @@ function getIdeaBox() {
   if (localStorage.length > 0) {
     var retrieved = localStorage.getItem('storedIdeas');
     ideaList = JSON.parse(retrieved);
-    ideaSection.innerHTML = '';
-    for (var i = 0; i < ideaList.length; i++) {
-      ideaSection.innerHTML += `
-      <article class="idea-box" id="${ideaList[i].id}">
-      <div class="icon-bar">
-      <img src="assets/star.svg" class="white-star" id="${ideaList[i].id}" alt="favorite star">
-      <img src="assets/delete.svg" class="delete-idea" id="${ideaList[i].id}" alt="delete idea">
-      </div>
-      <div class="title-body">
-      <h3>${ideaList[i].title}</h3>
-      <p>${ideaList[i].body}</p>
-      </div>
-      <div class="comment-bar">
-      <img src="assets/comment.svg" class="comment" id="add-comment" alt="add comment">
-      <h4>Comment</h4>
-      </div>
-      </article>`
-    }
+    reassignHTML();
   }
 };
 
@@ -127,5 +93,26 @@ function favoriteStar(targetClass) {
       ideaList[i].updateIdea();
       currentIdea.deleteFromStorage(stringifiedList, ideaList);
     }
+  }
+};
+
+function reassignHTML() {
+  ideaSection.innerHTML = '';
+  for (var i = 0; i < ideaList.length; i++) {
+    ideaSection.innerHTML += `
+    <article class="idea-box" id="${ideaList[i].id}">
+    <div class="icon-bar">
+    <img src="assets/star.svg" class="white-star" id="${ideaList[i].id}" alt="favorite star">
+    <img src="assets/delete.svg" class="delete-idea" id="${ideaList[i].id}" alt="delete idea">
+    </div>
+    <div class="title-body">
+    <h3>${ideaList[i].title}</h3>
+    <p>${ideaList[i].body}</p>
+    </div>
+    <div class="comment-bar">
+    <img src="assets/comment.svg" class="comment" id="add-comment" alt="add comment">
+    <h4>Comment</h4>
+    </div>
+    </article>`
   }
 };
